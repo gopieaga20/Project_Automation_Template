89e1a945b3442142235d4adf331b3dfe360b5c03
Gopinath Eaga <geaga@caesars.com>
2026-01-08 12:50:51 +0530

    extent report added in test runner

--- src/test/java/com/mtomics/runners/TestRunner.java
+++ src/test/java/com/mtomics/runners/TestRunner.java
@@ -8,12 +8,23 @@ import org.junit.runner.RunWith;
  * TestRunner class for executing Cucumber tests
  */
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features", glue = { "com.mtomics.stepDefinitions",
-                "com.mtomics.hooks" }, plugin = {
-                                "pretty",
-                                "html:target/cucumber-reports/cucumber.html",
-                                "json:target/cucumber-reports/cucumber.json",
-                                "junit:target/cucumber-reports/cucumber.xml" }, monochrome = true, dryRun = false, tags = "@admin")
+@CucumberOptions(
+    features = "src/test/resources/features",
+    glue = {
+        "com.mtomics.stepDefinitions",
+        "com.mtomics.hooks"
+    },
+    plugin = {
+        "pretty",
+        "html:target/cucumber-reports/cucumber.html",
+        "json:target/cucumber-reports/cucumber.json",
+        "junit:target/cucumber-reports/cucumber.xml",
+        "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+    },
+    monochrome = true,
+    dryRun = false,
+    tags = "@admin"
+)
 public class TestRunner {
         // This class will be empty - Cucumber uses annotations to run tests
 }
